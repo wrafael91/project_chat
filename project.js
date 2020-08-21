b739911619c3ce430346e19c87fc82a659fb9ef6
wrafael91 <wrafaelmp91@gmail.com>
2020-08-20 20:28:28 -0500

    cambio estructura y hora

--- project.js
+++ project.js
@@ -1,13 +1,13 @@
 $( "li" ).hover(function() {
-      $( this ).toggleClass( "active" ).next().stop( true, true )
+    $( this ).toggleClass( "active" ).next().stop( true, true )
 });
 
 $("li").click(function() {
-    $(this).toggleClass()
+  $(this).toggleClass()
 });
 
 $("form").on('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 });
 
 const messages = [];
@@ -16,52 +16,55 @@ const $send = document.querySelector(".button-send");
 const $chat = document.querySelector(".chat");// contenedor
 
 $send.addEventListener("click", sendMessage);
-    
+  
 function sendMessage() {
-    const value = $input.value;
-    if (value !== "") {
-        const now = new Date();
-        const message = {
-            time: `${now.getHours()}:${now.getMinutes()}`,
-            content: value,
-            author: {
-                name: "Rafael",
-                image: "http://lorempixel.com/50/50/sports/",
-            },
-        };
-        printMessage(message);
-        messages.push(message);
+  const value = $input.value;
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+      minutes1 = "0" + minutes;
+  }
+  if (value !== "") {
+      const now = new Date();
+      const message = {
+          time: `${now.getHours()}:${minutes1}`,
+          content: value,
+          author: {
+              name: "Rafael",
+              image: "http://lorempixel.com/50/50/sports/",
+          },
+      };
+      printMessage(message);
+      messages.push(message);
 
-        $input.value = "";
-    }
+      $input.value = "";
+  }
 }
 
 
 
 
 function printMessage(message) {
-    const {author, time, content} = message;
-    const template = 
-    `<div class="chat">
-    <div class="user-photo"><img src="${author.image}"></div> 
-    <div class="chat-message">
-        <div>
-            <a href="#" class="user-name">${author.name}</a>
-            <span class="date">${time}</span>
-        </div>
-        <div>
-            <span>${content}</span>  
-        </div>
-    </div>      
-    </div>`;
-  $chat.innerHTML += template;
+  const {author, time, content} = message;
+  const template = 
+  `<div class="chat">
+  <div class="user-photo"><img src="${author.image}"></div> 
+  <div class="chat-message">
+      <div>
+          <a href="#" class="user-name">${author.name}</a>
+          <span class="date">${time}</span>
+      </div>
+      <div>
+          <span>${content}</span>  
+      </div>
+  </div>      
+  </div>`;
+$chat.innerHTML += template;
 }
 
 
 for (let i = 0; i < messages.length; i++) {
-    printMessage(messages[i]);
+  printMessage(messages[i]);
 }
-
 
 
 
